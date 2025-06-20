dec8dd91ce832e3ae7c7516f288963fdf0209c4e
Miaad Asjad <miaadasjad1514@gmail.com>
2025-06-20 11:38:50 +0500

    server file changing

--- server.js
+++ server.js
@@ -9,13 +9,11 @@ import path from 'path';
 import connectDB from './config/db.js';
 
 import './config/passport.js';
-import categoryRoutes from './routes/categoryRoutes.js'
+import categoryRoutes from './routes/categoryRoutes.js';
 import blogRoutes from './routes/blogRoutes.js';
 import authRoutes from './routes/authRoutes.js';
-
 import tokenRoutes from './routes/tokenRoutes.js';
 import userRoutes from './routes/userRoutes.js';
-
 
 dotenv.config();
 const app = express();
@@ -24,19 +22,33 @@ const app = express();
 connectDB();
 
 
+const allowedOrigins = [
+  process.env.CLIENT_URL_1,
+  process.env.CLIENT_URL_2,
+];
+
+
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true
+}));
+
+
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({
-  origin: process.env.CLIENT_URL,
-  credentials: true,
-}));
 app.use('/uploads', express.static(path.join(process.cwd(), 'uploads')));
 
 
 app.use(session({
   secret: process.env.SESSION_SECRET,
   resave: false,
-  saveUninitialized: false,
+  saveUninitialized: false
 }));
 app.use(passport.initialize());
 app.use(passport.session());
@@ -45,9 +57,8 @@ app.use(passport.session());
 app.use('/api/blogs', blogRoutes);
 app.use('/api/auth', authRoutes);
 app.use('/api/auth', tokenRoutes);
-app.use('/api/categories',categoryRoutes);
-app.use('/api/users', userRoutes)
-
+app.use('/api/categories', categoryRoutes);
+app.use('/api/users', userRoutes);
 
 app.get('/', (_req, res) => {
   res.send('API is running');
